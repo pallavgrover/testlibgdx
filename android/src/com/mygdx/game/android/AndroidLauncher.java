d05f76acbb2494741597ab5439fd971fe65ed0ac
pallavgrover <pallav.grover@gmail.com>
2015-05-26 16:13:37 +0530

    test

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.PepperMain;
+import com.mygdx.game.Game.Drop;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new PepperMain(), config);
+		initialize(new Drop(), config);
 	}
 }
